5ac6dae2bba5469e55b72774f183f30e21a099fa
Shubham Soni <shubsoni1804@outlook.com>
2020-03-29 19:25:20 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
+	
+	
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int *processSafeSequence;
+	int numberOfResources;
+	int **numberOfMaximumRequiredResources;
+	int *availableResources;
+	int **numberOfAllocatedResources;
+	int **neededResources;
+	int numberOfProcesses;
+	int processRanN = 0;	
 
+	pthread_mutex_t lockResources;
+	pthread_cond_t condition;
