067259adb18facc7a8d87e9006fa1a92613408ff
Shubham Soni <shubsoni1804@outlook.com>
2020-03-29 19:26:26 +0530

    Update file.c

--- file.c
+++ file.c
@@ -20,3 +20,70 @@
 
 	pthread_mutex_t lockResources;
 	pthread_cond_t condition;
+bool getSafeSeq();
+	void* processCode(void* );
+	
+	int main(int argc, char** argv) {
+		srand(time(NULL));
+	
+
+	        printf("How many Processes does one want to see for -->");
+	        scanf("%d", &numberOfProcesses);
+	
+
+	        printf("what number of Resources required for checking --> ");
+	        scanf("%d", &numberOfResources);
+	
+
+	        availableResources = (int *)malloc(numberOfResources * sizeof(*availableResources));
+	        printf("Available Resource's count is --> ");
+	        for(i=0; i<numberOfResources; i++)
+	                scanf("%d", &availableResources[i]);
+	
+
+	        numberOfAllocatedResources = (int **)malloc(numberOfProcesses * sizeof(*numberOfAllocatedResources));
+	        for(i=0; i<numberOfProcesses; i++)
+	                numberOfAllocatedResources[i] = (int *)malloc(numberOfResources * sizeof(**numberOfAllocatedResources));
+	
+
+	        numberOfMaximumRequiredResources= (int **)malloc(numberOfProcesses * sizeof(*numberOfMaximumRequiredResources));
+	        for(i=0; i<numberOfProcesses; i++)
+	                numberOfMaximumRequiredResources[i] = (int *)malloc(numberOfResources * sizeof(**numberOfMaximumRequiredResources));
+	
+
+	        // numberOfAllocatedResources
+	        printf("\n");
+		for(i=0; i<numberOfProcesses; i++) {
+	                printf("numberOfAllocatedResources to process %d is --> ", i+1);
+	                for(j=0; j<numberOfResources; j++)
+	                        scanf("%d", &numberOfAllocatedResources[i][j]);
+	        }
+	        
+			// maximum required availableResources
+	        printf("\n");
+	        for(i=0; i<numberOfProcesses; i++) {
+	                printf("maximumRequiredResources by process %d is --> ", i+1);
+	                for(j=0; j<numberOfResources; j++)
+	                        scanf("%d", &numberOfMaximumRequiredResources[i][j]);
+	        }
+
+		// calculate neededResources matrix
+	        neededResources = (int **)malloc(numberOfProcesses * sizeof(*neededResources));
+	        for(i=0; i<numberOfProcesses; i++)
+	                neededResources[i] = (int *)malloc(numberOfResources * sizeof(**neededResources));
+	
+
+	        for(i=0; i<numberOfProcesses; i++)
+	                for(j=0; j<numberOfResources; j++)
+	                        neededResources[i][j] = numberOfMaximumRequiredResources[i][j] - numberOfAllocatedResources[i][j];
+	
+
+		// get safe sequence
+		processSafeSequence = (int *)malloc(numberOfProcesses * sizeof(*processSafeSequence));
+	        for(i=0; i<numberOfProcesses; i++) processSafeSequence[i] = -1;
+	
+
+	        if(!getSafeSeq()) {
+					printf("This is an unsafe process or unsafe state, kindly check again Once. thnak you.");
+	                exit(-1);
+			
